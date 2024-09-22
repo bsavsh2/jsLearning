de450facdeb4e16a1891638191642dfb9301ce52
Bohdan Savshak <bohdan.savshak@gmail.com>
2024-09-22 15:23:05 +0300

    class example

--- ch2/hello.js
+++ ch2/hello.js
@@ -117,3 +117,102 @@ Coercion means a value of one type being converted to its respective representat
  */
 
 // ------------ How We Organize in JS --------------
+
+// Classes
+class Page {
+    constructor(text) {
+        this.text = text;
+    }
+
+    print() {
+        console.log(this.text);
+    }
+}
+
+class Notebook {
+    constructor() {
+        this.pages = []
+    }
+
+    addPage(text) {
+        var page = new Page(text);
+        this.pages.push(page);
+    }
+
+    print() {
+        for (let page of this.pages) {
+            page.print();
+        }
+    }
+}
+
+
+var mathNotes = new Notebook();
+mathNotes.addPage("Javascript");
+mathNotes.addPage("Hello Math");
+mathNotes.print();
+
+
+class Publication {
+    constructor(title, author, pubDate) {
+        this.title = title;
+        this.author = author;
+        this.pubDate = pubDate;
+    }
+
+    print() {
+        console.log(`
+            Title: ${this.title}
+            Author: ${this.author}
+            PubDate: ${this.pubDate}
+        `);
+    }
+}
+
+class Book extends Publication {
+    constructor(bookDetails) {
+        super(bookDetails.title, bookDetails.author, bookDetails.publishedOn);
+        this.publisher = bookDetails.publisher;
+        this.ISBN = bookDetails.ISBN;
+    }
+
+    print() {
+        super.print();
+        console.log(`
+            Publisher: ${this.publisher}
+            ISBN: ${this.ISBN}
+        `);
+    }
+}
+
+class BlogPost extends Publication {
+    constructor(title, author, pubDate, URL) {
+        super(title, author, pubDate);
+        this.URL = URL;
+    }
+
+    print() {
+        super.print();
+        console.log(`
+            URL: ${this.URL}
+        `);
+    }
+}
+
+var YDKJS = new Book({
+    title: "You Don't Know JS",
+    author: "Kyle Simpson",
+    publishedOn: "June 2014",
+    publisher: "O'Reilly",
+    ISBN: "123456-789"
+})
+
+YDKJS.print();
+
+var forAgainsLet = new BlogPost(
+    "For and agains let",
+    "Kyle Simpson",
+    "October 27, 2014",
+    "https://davidwalsh.name/for-and-agains-let"
+)
+forAgainsLet.print();
