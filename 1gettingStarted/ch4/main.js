308e752655c4325d332aa62499703bf8517f1f80
Bohdan Savshak <bohdan.savshak@gmail.com>
2024-09-26 21:11:00 +0300

    create another directory for second book

--- 1gettingStarted/ch4/main.js
+++ 1gettingStarted/ch4/main.js
@@ -7,4 +7,10 @@ prototypal class - is interesting thing. ES6 class works on top of it.
 Idea is with new you link newly created object's prototype to Function itself.
 So prototypal delegation will work.
 
+
+Next Books:
+2. Scope and Clojure
+3. this and Class
+4. Coercion and types
+
  */
