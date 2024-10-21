61eb37377c52130bf269380e88d840ca116d9aa7
Bohdan Savshak <bohdan.savshak@gmail.com>
2024-10-21 19:59:30 +0300

    Started Chapter 7: Closures

---
+++ 2scopeAndClojure/ch7/main.js
@@ -0,0 +1,7 @@
+/*
+Chapter 7: Using Closures
+
+Our broad goal in
+this book is not merely to understand scope, but to more effectively use it
+in the structure of our programs; closure is central to that effort.
+ */
