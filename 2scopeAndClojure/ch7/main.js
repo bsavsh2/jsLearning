946e2998a3a865544aef762ce339cbb1c303dae0
Bohdan Savshak <bohdan.savshak@gmail.com>
2024-10-24 20:31:41 +0300

    Finish Chapter 7: Using Closures.

--- 2scopeAndClojure/ch7/main.js
+++ 2scopeAndClojure/ch7/main.js
@@ -7,6 +7,52 @@ in the structure of our programs; closure is central to that effort.
 
 Closure gives a function access to its outer scope.
 A Closure is the combination of a function bundled together(enclosed) with references to its surrounding state.
+
+Closure is the set of free variables from the environment of your lambda that need to be saved somewhere.
+lambda(x), x + 2 - closed lambda expression. It has everything it needs to execute. It doesn't need anything from
+ environment.
+lambda(x), x/y + 2 - open lambda expression because it needs to get free variable in order to execute.
+
+Closure is the set of free variables from the environment of your lambda that need to be saved somewhere to make
+ your open lambda closed.
+
+
+ !!!!!!!!!!!!!!!!
+ As I explained above, the closure of a lambda expression is the subset of definitions in its environment that
+  give values to the free variables contained in that lambda expression,
+  effectively closing the expression (turning an open lambda expression, which cannot be evaluated yet,
+  into a closed lambda expression, which can then be evaluated, since all the symbols contained in it are now defined).
+
+
+  https://stackoverflow.com/questions/220658/what-is-the-difference-between-a-closure-and-a-lambda
+
+  There are two ways of explaining what closure is.
+  From more practical way of thinking where your function "closes over"/"encloses" outer variables.
+  Or from original though of open/closed lambda expression with free variables.
+
+
+
+
+  Kyle's definition
+  Closure is the link-association that connects that
+    function to the scope/variables outside of itself, no matter where that
+    function goes.
+
+
+    In js closure is implemented as a function reference. And you just pass the reference of a function not function
+     itself. And because function stays in the same place it still has access to outer variables through scope chain.
+     That's perspective that Kyle highlighted.
+     I like more to think about closure as open function with free variables that was closed. and Implementation
+      details don't really matter. Closure = function + environment.
+
+
+We explored two models for mentally tackling closure:
+    - Observational: closure is a function instance remembering its outer
+    variables even as that function is passed to and invoked in other
+    scopes.
+    - Implementational: closure is a function instance and its scope
+    environment preserved in-place while any references to it are passed
+    around and invoked from other scopes.
  */
 
 function adder(num1) {
